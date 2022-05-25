c49b17bfc457d4f17ef5e1f31e105cdcd3a3c9ed
Médéric <mederic@2T0QTY1.localdomain>
2022-05-25 09:31:36 -0400

    Added substrata classification; added function comments; acer classification

--- predict_sub.py
+++ predict_sub.py
@@ -16,11 +16,12 @@ import pandas as pd
 
 checkpoint_no ="transfer_1200_npp_trip_deep"
 checkpoint_abb_pic = "transfer_abb_pic_sigmoid"
+checkpoint_acer = ""
 checkpoint_pinus = "transfer_pinus_sigmoid_deep"
 checkpoint_tricolp = "tricolp_no_val_sig_normal_depth"
 checkpoint_tripor = "transfer_tripor_small_deep"
 checkpoint_path = "checkpoints/"+checkpoint_no+"/cp-{epoch:04d}.ckpt"
-data_path = os.getcwd()+"/images_pre_1002_1065"
+data_path = os.getcwd()+"/images_pre_small"
 data_path_post = os.getcwd()+"/images_post_class"
 
 # 280_475
@@ -50,7 +51,7 @@ classes_select = sorted(classes_select)
 threshold = 0.8
 presence = True #could be used to filter if a certain level needs to be classified
 max_samples = 60000
-local_classification = False #If True, locally copies the classified data to its own classification folder
+local_classification = True #If True, locally copies the classified data to its own classification folder
 
 local_classification_overwrite = True #This should be left on
 lvl_1_switch = True      #This should be left on 
@@ -89,6 +90,7 @@ lbl_dict = {'abb_pic_mix':["abies_b", "picea_mix"],
             'pinus_mix':['pinus_b_mix', 'pinus_s'],
             'tricolp_mix':['acer_mix', 'quercus_r'],
             'tripor_mix':['alnus_mix', 'betula_mix', 'corylus_c', 'eucalyptus'],
+            'acer_mix':['acer_r', 'acer_s']
 
     }
   
@@ -102,9 +104,11 @@ for label in lbl_pretty:
     if label in lbl_dict:
         for sub_label in lbl_dict[label]:
             lbl_final.append(sub_label)
-    
-    # if label not in lbl_dict:
-    #     lbl_final.append(label)
+            
+            if sub_label in lbl_dict:   #add lvl_3 classes
+                for sub_sub_label in lbl_dict[sub_label]:
+                    lbl_final.append(sub_sub_label) 
+
 
 lbl_final.append('unknown')
 lbl_final = sorted(lbl_final)
@@ -261,6 +265,9 @@ def load_from_sub(directory, extension, width, norm, min_count=20):
 
 #%%
 # Definition of classification
+# This function takes the first run of predictions as an input and their corresponding filenames
+# if local_classification is True; the function will then locally copy every file to its corresponding folder
+# In both cases, this function will increment the compilation numpy array
 def run_local_classification(directory, lbl_list, prediction_data, filenames_list):
     
     filename_it = 0
@@ -284,17 +291,16 @@ def run_local_classification(directory, lbl_list, prediction_data, filenames_lis
         print("Classifying depth "+str(current_depth))
         current_depth_folder = data_path_post+"/"+checkpoint_no+"/"+str(current_depth)
         
-        if lvl_1_switch == True:    #create a sub folder for each depth level if this is the first classification round
+        if lvl_1_switch == True:    #create a sub folder for each depth level if this is the first classification run
             
             os.makedirs(current_depth_folder)
             os.makedirs(current_depth_folder+'/unknown')
             
-        for lbl in lbl_list:
+        for lbl in lbl_list:    #create a sub folder for the local classification of each taxa
             os.makedirs(current_depth_folder+'/'+lbl)
         
         for pollen in prediction_data:
             
-            # print(str(pollen[len(lbl_list)]) + " is equal to " + str(current_depth))
             
             if pollen[-1] == current_depth: # -1 is the last element in an np array
                 
@@ -330,28 +336,30 @@ def run_local_classification(directory, lbl_list, prediction_data, filenames_lis
                 
                 filename_it = filename_it+1
                 
-            elif pollen[-1] > current_depth: break
+            elif pollen[-1] > current_depth: break # Go to next depth
 
 
 
 #%%
-def create_masterlist (checkpoint_level):
-    
-    # latest = tf.train.latest_checkpoint("checkpoints/"+checkpoint_level)
-    # latest = ("checkpoints/"+checkpoint_no+"/cp-0300.ckpt") #Checkpoint en particulier?
-    
-    # second_cnn = model.load_weights(latest)
-    
+def create_masterlist (prediction_data):
+
+    #This function takes in the prediction data as input
+    #It creates a numpy array (masterlist_lvl2) of every datapoint assigned to a class that contains a substrata
+    # ... and that is over the unknown threshold
+    # [filename][prediction]
+    # Outputs the masterlist, the corresponding image data and their depth
+
+
     copy_it = 0
     copy_index = []
     pollen_index = []
     filenames_np = np.asarray(filenames)
-    # masterlist_lvl2 = np.asarray()
+
     
-    
-    for pollen in predictions:
-        if lbl_pretty[pollen[0:len(lbl_pretty)].argmax()] in lbl_dict and pollen[0:len(lbl_pretty)].max() > threshold:
-            # WE DONT WANT TO CLASSIFY UNKNOWN POLLENS ON THE SECOND CLASSIFICATION RUN
+    for pollen in prediction_data:
+        if lbl_pretty[pollen[0:len(lbl_pretty)].argmax()] in lbl_dict \
+        and pollen[0:len(lbl_pretty)].max() > threshold:
+            
             copy_index.append(copy_it)
             pollen_index.append(str(lbl_pretty[pollen[0:len(lbl_pretty)].argmax()]))
             
@@ -376,9 +384,14 @@ def create_masterlist (checkpoint_level):
 #%%
 def sub_level_prediction (masterlist, images, branch, checkpoint_sub, depth_index):
     
-    #Create sub_image list
+    #Predicts the data from an upper level branch/node into substrata
+    #Inputs: the branch name (string), the masterlist, the image data with lvl<1, their depth
+    #Predicts on the corresponding images
+    #Outputs prediction data for this branch
     
-    index_extract = np.where(masterlist == branch)[0] #Change this
+    #Create sub_image list from masterlist
+    
+    index_extract = np.where(masterlist == branch)[0] #[filename][prediction]
     images_extract = np.copy(images_lvl2[index_extract,])
     filenames_extract = np.copy(filenames_lvl2[index_extract,])
     filenames_extract = filenames_extract.flatten()
@@ -407,6 +420,20 @@ def sub_level_prediction (masterlist, images, branch, checkpoint_sub, depth_inde
 
     return predictions_lvl2
 
+#%%
+def update_masterlist (masterlist, prediction_data, branch):
+    
+    
+    
+    i = 0
+
+    for pollen in masterlist:
+        if pollen[1] == branch:
+            pollen[1] = lbl_dict[branch][int(prediction_data[i,0])]
+            i = i+1
+
+    return masterlist
+                                         
 
 #%%
 
@@ -426,6 +453,9 @@ for layer in base_model.layers[0:3]:
     layer.trainable = False
 
 base_model.trainable = False
+
+#%%
+os.environ['KMP_DUPLICATE_LIB_OK']='True' #PaleoMacOS only
 
 #%% 
 #########################
@@ -486,7 +516,7 @@ lvl_1_switch = False
 
 #%%
 copy_index, images_lvl2, filenames_lvl2, pollen_index, \
-depth_index_lvl2, masterlist_lvl2 = create_masterlist(checkpoint_no) #run second prediction from lvl 2 pollens
+depth_index_lvl2, masterlist_lvl2 = create_masterlist(predictions)
 
 #%%
 ####################################
@@ -532,6 +562,9 @@ model.add(tf.keras.layers.Dense(len(lbl_dict['abb_pic_mix']), activation = ac_fu
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 predictions_abb_pic = sub_level_prediction(masterlist_lvl2, images_lvl2, "abb_pic_mix", checkpoint_abb_pic, depth_index_lvl2)
+
+masterlist_lvl3 = update_masterlist(masterlist_lvl2, predictions_abb_pic, 'abb_pic_mix')
+
 #%%
 
 #%%
@@ -581,6 +614,7 @@ model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 predictions_pinus = sub_level_prediction(masterlist_lvl2, images_lvl2, "pinus_mix", checkpoint_pinus, depth_index_lvl2)
 
+masterlist_lvl3 = update_masterlist(masterlist_lvl3, predictions_pinus, 'pinus_mix')
 
 #%%
 ####################################
@@ -621,11 +655,14 @@ model.add(tf.keras.layers.MaxPooling2D())
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dropout(0.5,seed=7))
 model.add(tf.keras.layers.Dense(512, activation='relu'))
-model.add(tf.keras.layers.Dense(len(lbl_dict['tricolp_mix']), activation = ac_function))
+# model.add(tf.keras.layers.Dense(len(lbl_dict['tricolp_mix']), activation = ac_function))
+model.add(tf.keras.layers.Dense(1, activation = ac_function))
 
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 predictions_tricolp = sub_level_prediction(masterlist_lvl2, images_lvl2, "tricolp_mix", checkpoint_tricolp, depth_index_lvl2)
+
+masterlist_lvl3 = update_masterlist(masterlist_lvl3, predictions_tricolp, 'tricolp_mix')
 
 #%%
 ####################################
@@ -674,10 +711,55 @@ model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['a
 
 predictions_tripor = sub_level_prediction(masterlist_lvl2, images_lvl2, "tripor_mix", checkpoint_tripor, depth_index_lvl2)
 
+masterlist_lvl3 = update_masterlist(masterlist_lvl3, predictions_tripor, 'tripor_mix')
+
+#%%
+####################################
+#CLASSIFY ACER
+####################################
+
+print("Classifying Acer")
+
+del model# THERE COULD BE OTHER THINGS TO DELETE/ RESET THE SEED / RESET DEFAULT GRAPH
+del base_model
+
+tf.keras.backend.clear_session()
+tf.compat.v1.reset_default_graph()
+#reset_seeds()
 
 
+base_model = tf.keras.applications.VGG16(input_shape = (resolution, resolution, 3),
+                                          include_top = False,
+                                          weights = 'imagenet')
 
+#Lock layers
+for layer in base_model.layers[0:3]:
+    layer.trainable = False
 
+base_model.trainable = False
+
+ac_function = 'sigmoid'
+
+model = tf.keras.models.Sequential()
+
+model.add(base_model.layers[3])
+model.add(tf.keras.layers.Conv2D(128, (3,3), activation='relu', padding='same'))
+model.add(tf.keras.layers.Conv2D(128, (3,3), activation='relu', padding='same'))
+model.add(tf.keras.layers.MaxPooling2D())
+model.add(tf.keras.layers.Conv2D(256, (3,3), activation='relu', padding='same'))  # additional layer for 128x128
+model.add(tf.keras.layers.Conv2D(256, (3,3), activation='relu', padding='same'))  # additional layer for 128x128
+model.add(tf.keras.layers.MaxPooling2D())    
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dropout(0.5,seed=7))
+model.add(tf.keras.layers.Dense(512, activation='relu'))
+# model.add(tf.keras.layers.Dense(len(lbl_dict['tricolp_mix']), activation = ac_function))
+model.add(tf.keras.layers.Dense(1, activation = ac_function))
+
+model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+predictions_acer = sub_level_prediction(masterlist_lvl3, images_lvl2, "acer_mix", checkpoint_tricolp, depth_index_lvl2)
+
+masterlist_lvl4 = update_masterlist(masterlist_lvl3, predictions_tricolp, 'acer_mix')
 
 
 #%%
